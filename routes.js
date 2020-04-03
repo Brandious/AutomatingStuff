810de6f8f28171af396e673fbd7de15202ce8d19
brandious <adnanfacic96@gmail.com>
2020-04-04 01:44:12 +0200

    refactoring

--- routes.js
+++ routes.js
@@ -7,57 +7,58 @@ let list = [];
 let returnArray = [];
 
 router.get("/", (req, res) => {
-  res.render('pages/index', {list:list ,returnList:returnArray, check: false});
+  res.render("pages/index", {
+    list: list,
+    returnList: returnArray,
+    check: false
+  });
 });
 
-
-async function runScript()
-{
+async function runScript() {
   let x = await execShellCommand("python3 second.py");
   console.log(1);
- 
+
   return x;
 }
 
-router.post("/automate", async(req, res) => {
-  return res.status(200).send({list: list,returnList: returnArray, check:false})
-})
+router.post("/automate", async (req, res) => {
+  return res
+    .status(200)
+    .send({ list: list, returnList: returnArray, check: false });
+});
 
-router.post("/artikli", async(req, res) => {
+router.post("/artikli", async (req, res) => {
+  let tab = JSON.parse(req.body.table);
+  returnArray = tab;
+  console.log(tab);
 
-    let tab = JSON.parse(req.body.table);
-    returnArray = tab;
-    console.log(tab);
-    
-    const returnList = list[list.length-1].filter((el) => {
-      return !tab.some((f) => {
-        if(f === null) return false;
-        return f.title === el.title;
-      });
+  const returnList = list[list.length - 1].filter(el => {
+    return !tab.some(f => {
+      if (f === null) return false;
+      return f.title === el.title;
     });
-    
+  });
 
-    list[list.length-1] = returnList;
-    return res.status(200).send({list: returnList, returnList: returnArray ,check: true});
-
-})
-
+  list[list.length - 1] = returnList;
+  return res
+    .status(200)
+    .send({ list: returnList, returnList: returnArray, check: true });
+});
 
 router.get("/artikli", async (req, res) => {
   console.log("=> /Artikli");
- // let y = await runScript(); 
- // if(y)
- // {
-      fs.readFile("test.json", "utf8", (err, data) => {
-        console.log(2)
-        if (err) throw err;
-        let art = JSON.parse(data);
+  let y = await runScript();
+  if (y) {
+    fs.readFile("test.json", "utf8", (err, data) => {
+      console.log(2);
+      if (err) throw err;
+      let art = JSON.parse(data);
 
-        list.push(art);
-      });
-  // }
+      list.push(art);
+    });
+  }
 
-  res.send({list:list,returnList:returnArray ,check: true});
+  res.send({ list: list, returnList: returnArray, check: true });
 });
 
 module.exports = router;
